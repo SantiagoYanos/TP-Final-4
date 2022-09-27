db37beab4882cc816a573c4ff3f1bc3a5e01c225
Agustin <69488102+agustingk@users.noreply.github.com>
2022-09-27 10:39:54 -0300

    Act. Styles

--- visuals/register_guardian.php
+++ visuals/register_guardian.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/styleRegister.css">
+    <link rel="stylesheet" href="\TP-Final-4\css\styleRegister.css">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Nabla&display=swap" rel="stylesheet">
     <title>Guardian Register</title>
