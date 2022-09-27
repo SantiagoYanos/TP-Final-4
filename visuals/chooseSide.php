a1f13fca9eb9cd95962a0a19e806e6f4822bc9f8
Yanos <santiago.yanos@gmail.com>
2022-09-27 09:32:36 -0300

    Change: etc -> images

---
+++ visuals/chooseSide.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Register Side</title>
+</head>
+<body>
+
+<div class="row">
+  <div class="col">
+    Columna
+  </div>
+  <div class="col">
+    Columna
+  </div>
+</div>
+
+
+
+
+    
+</body>
+</html>
