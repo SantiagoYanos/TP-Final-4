5bb4e8dfbbc5a783017187750a3a6f7dd50fdb8e
Yanos <santiago.yanos@gmail.com>
2022-09-27 10:51:46 -0300

    Add: "Visual chooseSide.php"

--- visuals/chooseSide.php
+++ visuals/chooseSide.php
@@ -6,17 +6,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="../css/chooseSide.css" rel="stylesheet">
     <title>Register Side</title>
 </head>
 <body>
 
-<div class="row">
-  <div class="col">
-    Columna
+<div class="row text-center">
+  <div class="col-sm mt-5 border-end border-4 border-dark">
+    <h1 class="role-title">Guardian<h1>
+      <a href="/registerGuardian"><img class="img-fluid role-image mt-3" src="../images/Guardian.png"></a>
   </div>
-  <div class="col">
-    Columna
+  <div class="col-sm mt-5">
+    
+    <h1 class="role-title">Pet Owner</h1>
+    <a href="registerOwner"><img class="img-fluid role-image mt-3" src="../images/Pet_Owner.png"></a>
+    
   </div>
+</div>
+<div class="row"> 
+  
+<div class="col text-center">
+
+<div class="container align-items-center border border-dark chooseYourSide">
+
+  <b class="f1">CHOOSE YOUR SIDE</b>
+
+</div>
+
+</div>
+
 </div>
 
 
