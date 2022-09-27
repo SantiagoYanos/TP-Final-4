842cf8b964f7bcaf4d687812862ba974422236e2
Agustin <69488102+agustingk@users.noreply.github.com>
2022-09-27 09:27:12 -0300

    Register Guardian

---
+++ register_guardian.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Guardian Register</title>
+</head>
+<body>
+    <h1>Guardian Register</h1>
+    <h2>Add your info: </h2><br>
+
+    <form action="..." method="post">
+        <label for="name">Name: </label>
+        <input type="text" name="name" placeholder="Name"></br>
+        <label for="">Last Name: </label>
+        <input type="text" name="last_name" placeholder="Last Name"></br>
+        <label for="">Email: </label>
+        <input type="email" name="email" placeholder="Email"></br>
+        <label for="">Password: </label>
+        <input type="password" name="password" placeholder="Password"></br>
+        <label for="">Adress: </label>
+        <input type="text" name="adress" placeholder="Adress"></br>
+        <label for="">Phone: </label>
+        <input type="phone" name="phone" placeholder="Phone"></br>
+
+        <label for="pet_size">Pet Size Preference: </label>
+        <select name="pet_size" id="pet_size">
+            <option value="small">Small</option>
+            <option value="medium">Medium</option>
+            <option value="big">Large</option>
+        </select></br>
+
+        <label for="birthdate">Birthdate: </label>
+        <input type="date" name="birthdate"></br>
+
+        <input type="checkbox" name="terms_conditions" value="terms_conditions"></input>
+        <a href="/etc/rick-roll.gif">I agree to the terms and conditions</a></br>
+    
+        <button type="submit">Register</button>
+    </form>
+</body>
+</html>
