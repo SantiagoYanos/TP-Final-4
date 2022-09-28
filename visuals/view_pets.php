7f6bbd4b7c04404fb0426c70f512132a8a65bed2
Manuel <manugrassi9@gmail.com>
2022-09-28 11:35:07 -0300

    view pets 0.1

---
+++ visuals/view_pets.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>view_pets</title>
+</head>
+<body>
+<h1>My pets</h1>
+
+<table style="text-align:center;" border="2">
+            <thead>
+                <tr>
+                    
+                    <th style="width: 150px;">photo</th> 
+                    <th style="width: 150px;">name</th> 
+                    <th style="width: 150px;">breed</th> 
+                    <th style="width: 50px;">size</th> 
+                    <th style="width: 150px;">vaccination note</th> 
+                    <th style="width: 250px;">observations</th> 
+                    <th style="width: 150px;">video</th> 
+                    <th style="width: 150px;">select</th> 
+                   
+                </tr>
+
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td><img src="../images/rick-roll.gif" alt="rick" height="100" width="100" > </td>
+                    <td>firulais </td>
+                    <td>golden </td>
+                    <td>big </td>
+                    <td><img src="../images/Pet_Owner.png" alt="vac note"height="100" width="100" ></td>
+                    <td>very good doggo </td>
+                    <td><img src="../images/rick-roll.gif" alt="pet video" height="100" width="100"> </td>
+                    <td><button type="submit">select</button></td>
+                </tr>
+                <tr>
+                <td><img src="../images/rick-roll.gif" alt="rick" height="100" width="100" > </td>
+                    <td>firulais </td>
+                    <td>golden </td>
+                    <td>big </td>
+                    <td><img src="../images/Pet_Owner.png" alt="vac note" height="100" width="100" ></td>
+                    <td>very good doggo </td>
+                    <td><img src="../images/rick-roll.gif" alt="pet video" height="100" width="100"> </td>
+                    <td><button type="submit" >select</button></td>
+                </tr>
+
+            </tbody>
+            
+
+   
+
+
+        </table>
+        <button type="submit">Add new pet</button> <button type="submit">edit</button> <button type="submit">remove</button>
+
+    
+</body>
+</html>
