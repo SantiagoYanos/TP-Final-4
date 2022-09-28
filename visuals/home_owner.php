906885dd3c89befc095760a29fad347f7cffea21
Agustin <69488102+agustingk@users.noreply.github.com>
2022-09-28 11:17:24 -0300

    Owner's Home

---
+++ visuals/home_owner.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <div>
+        <h1>Owner's Home</h1>
+    </div>
+
+    <div>
+        <form action="..." method="post">
+            <button type="submit">View Pets</button></br>
+        </form>
+    </div>
+    
+    <div>
+        <form action="..." method="post">
+            <button type="submit">Visualize Guardians</button></br>
+        </form>
+    </div>
+
+    <div>
+        <form action="..." method="post">
+            <button type="submit">See Reservations</button>
+        </form>
+    </div>
+
+    </br></br>
+
+    <div>
+        <h2>Owner's Information</h2>
+    </div>
+
+    <table>
+        <tr>
+            <th>Name</th>
+        </tr>
+
+        <tr>
+            <th>Last Name</th>
+        </tr>
+
+        <tr>
+            <th>Adress</th>
+        </tr>
+        
+        <tr>
+            <th>Phone</th>
+        </tr>
+
+        <tr>
+            <th>Password</th>
+        </tr>
+
+        <tr>
+            <th>Birth Date</th>
+        </tr>
+
+    </table>
+
+</body>
+</html>
