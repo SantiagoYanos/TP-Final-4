487d5731c840509afd5c240d848a6ba919bd5018
Yanos <santiago.yanos@gmail.com>
2022-09-28 11:46:18 -0300

    Add: homeGuardian.php

---
+++ visuals/homeGuardian.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://kit.fontawesome.com/dec9278e05.js" crossorigin="anonymous"></script>
+    <title>Guardian Home</title>
+</head>
+<body>
+    
+    <h1>Guardian's home</h1> <i class="fa fa-house"></i>
+
+    <h2>Reputation</h2>
+
+    <div class="container">
+
+   
+    <b>0 stars</b>
+    <b class="float-end">5 stars</b>
+
+    <div class="progress mb-3">
+        <div class="progress-bar" style="width:70%"></div>
+    </div>
+
+    <div>
+        <div>
+            <input type="date"> <button> Add </button>
+        </div>
+
+        <table class="table table-striped table-bordered">
+            <tr>
+                <th>Available Dates</th>
+                <th>Action</th>
+            </tr>
+            <tr>
+                <td name="fecha">Alfreds Futterkiste</td>
+                <td name="select">Maria Anders</td>
+            </tr>
+        </table>
+
+        <button>Check Petitions</button>
+
+        <button class="float-end">Remove</button>
+
+    </div>
+
+    <div class="container align-items-end">
+
+        <table class="table table-striped table-bordered mt-2">
+
+                <tr>
+                    <th>Name</th>
+                    <td>Rogelio</td>
+                </tr>
+
+                <tr>
+                    <th>Last Name</th>
+                    <td>Salame</td>
+                </tr>
+
+                <tr>
+                    <th>Adress</th>
+                    <td>Alberti 2051</td>
+                </tr>
+
+                <tr>
+                    <th>Phone</th>
+                    <td>2235 123456</td>
+                </tr>
+
+                <tr>
+                    <th>Password</th>
+                    <td>********</td>
+                </tr>
+
+                <tr>
+                    <th>Pet size reference</th>
+                    <td>Big</td>
+                </tr>
+
+                <tr>
+                    <th>Birth Date</th>
+                    <td>June 24, 1917</td>
+                </tr>
+
+        </table>
+
+        <button class="float-end">Edit</button>
+
+    </div>
+
+    <button>Log out</button>
+
+</body>
+</html>
