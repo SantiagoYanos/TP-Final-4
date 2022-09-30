045f7179e96f78de47c7d390d4b1b2b0f32e35f3
Manuel <manugrassi9@gmail.com>
2022-09-30 10:51:35 -0300

    Cp update 0.4 framework

---
+++ Config/Config.php
@@ -0,0 +1,10 @@
+<?php namespace Config;
+
+define("ROOT", dirname(__DIR__) . "/");
+//Path to your project's root folder
+define("FRONT_ROOT", "/UTN/TP6/ExerciseThree/");
+define("VIEWS_PATH", "Views/");
+define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "css/");
+define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/");
+define("IMG_PATH", VIEWS_PATH . "img/");
+?>
