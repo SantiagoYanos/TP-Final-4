81417f66bda5f1e08981f82128bdb3cf620781d8
Yanos <santiago.yanos@gmail.com>
2022-09-30 11:50:10 -0300

    Add: guardianList.css

--- Views/guardianList.php
+++ Views/guardianList.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
-    <link href="css/guardianList.css" rel="stylesheet">
+    <link href="./css/guardianList.css" rel="stylesheet">
     <title>Guardian List</title>
 </head>
 <body>
