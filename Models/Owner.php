0cd6aa43fafcac2580f399cfa7b9c5fd599e40a8
Yanos <santiago.yanos@gmail.com>
2022-10-04 09:38:44 -0300

    Add: Models/Owner.php

---
+++ Models/Owner.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Models;
+
+use Models\Owner as Owner;
+
+class Owner {
+    private $name;
+    private $dni;
+    private $phone;
+    private $pets;
+    private $email;
+    private $password;
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this->name;
+    }
+
+    public function getDni()
+    {
+        return $this->dni;
+    }
+
+    public function setDni($dni)
+    {
+        $this->dni = $dni;
+
+        return $this->dni;
+    }
+
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+
+        return $this->phone;
+    }
+
+    public function getPets()
+    {
+        return $this->pets;
+    }
+
+    public function setPets($pets)
+    {
+        $this->pets = $pets;
+
+        return $this->pets;
+    }
+
+    public function getEmail()
+    {
+        return $this->$email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this->email;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this->password;
+    }
+}
+
+?>
