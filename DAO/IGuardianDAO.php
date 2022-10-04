2667a4d020708aa593d9e8cd944c652e8f01f4b0
Manuel <manugrassi9@gmail.com>
2022-10-04 10:13:58 -0300

    IguardianDao 0.1

--- DAO/IGuardianDAO.php
+++ DAO/IGuardianDAO.php
@@ -1,6 +1,15 @@
 <?php
     namespace DAO;
-    
+
+    use Models\Guardian as Guardian;
+
+    interface IGuardianDAO 
+    {
+        function Add(Guardian $guardian);
+        function GetAll();
+        function GetById($id);
+        function Remove($id);
+    }
 
 
 
