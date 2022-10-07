3583b6e24c18cb61ff0a063e79407e2460bb4fe8
Agustin <69488102+agustingk@users.noreply.github.com>
2022-10-07 12:28:14 -0300

    AuthController

--- Controllers/AuthController.php
+++ Controllers/AuthController.php
@@ -4,12 +4,70 @@ namespace Controllers;
 
 use DAO\GuardianDAO as GuardianDAO;
 use DAO\OwnerDAO as OwnerDAO;
+use Models\Guardian as Guardian;
+use Models\Owner as Owner;
 
 class AuthController
 {
-
-    public function Register()
+    public function RegisterOwner($id, $name, $dni, $phone, $email, $password)
     {
+        $ownerDAO = new OwnerDAO;
+        $guardianDAO = new GuardianDAO;
+
+        if($ownerDAO->getByEmail($email) || $guardianDAO->getByEmail($email)){
+            ///redirigir al index;
+            ///existe el owner o guardian con el email ingresado;
+            return require_once (ROOT);
+        }
+        else{
+            $owner = new Owner();
+
+            $owner->setId($id);
+            $owner->setName($name);
+            $owner->setDni($dni);
+            $owner->setPhone($phone);
+            $owner->setPets(array());
+            $owner->setEmail($email);
+            $owner->setPassword($password);
+
+            $ownerDAO->add($owner);
+
+            ///creamos la cuenta de owner.
+
+            return require_once (ROOT);
+        }
+    }
+
+    public function RegisterGuardian($id, $cuil, $name, $adress, $price, $phone, $prefered_size, $email, $password, $available_date){
+        $ownerDAO = new OwnerDAO;
+        $guardianDAO = new GuardianDAO;
+
+        if($ownerDAO->getByEmail($email) || $guardianDAO->getByEmail($email)){
+            ///redirigir al index;
+            ///existe el owner o guardian con el email ingresado;
+            return require_once (ROOT);
+        }
+        else{
+            $guardian = new Guardian();
+            
+            $guardian->setId($id);
+            $guardian->setCuil($cuil);
+            $guardian->setName($name);
+            $guardian->setAdress($adress);
+            $guardian->setPhone($phone);
+            $guardian->setPrefered_size($prefered_size);
+            $guardian->setReputation("3");
+            $guardian->setPrice($price);
+            $guardian->setEmail($email);
+            $guardian->setPassword($password);
+            $guardian->setAvailable_date($available_date);
+
+            $guardianDAO->Add($guardian);
+
+            ///creamos la cuenta de guardian.
+
+            return require_once (ROOT);
+        }
     }
 
     public function Login($email, $password)
@@ -19,7 +77,39 @@ class AuthController
 
         $user = $guardian_DAO->GetByEmail($email);
 
-    public function Login($email, $password)
-    {
+        if ($user) {
+            if ($user->getPassword() == $password) {
+
+                //Crear sesión
+                session_start();
+
+                $_SESSION["email"] = $user->getEmail();
+
+                $_SESSION["type"] = "guardian";
+
+                //Redirigir a perfil Guardian (return)
+                return require_once("VIEWS_PATH" . "home_guardian.php");
+            }
+        }
+
+        $user = $owner_DAO->getByEmail($email);
+
+        if ($user) {
+            if ($user->getPassword() == $password) {
+
+                //Crear sesión
+                session_start();
+
+                $_SESSION["email"] = $user->getEmail();
+
+                $_SESSION["type"] = "owner";
+
+                //Redirigir a perfil Owner (return)
+                return require_once("VIEWS_PATH" . "home_owner.php");
+            }
+        }
+
+        //Redirigir a Login otra vez (return)
+        return require_once("VIEWS_PATH" . "login.php");
     }
 }
