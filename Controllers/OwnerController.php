adbcce8888feb2a436261cb102ca0304922bc435
Yanos <santiago.yanos@gmail.com>
2022-10-10 17:35:43 -0300

    Add: HomeOwner

---
+++ Controllers/OwnerController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Controllers;
+
+use DAO\OwnerDAO as OwnerDAO;
+
+class OwnerController
+{
+    public function HomeOwner()
+    {
+        require_once(FRONT_ROOT . "/Utils/validateSession.php");
+
+        $owner_DAO = new OwnerDAO();
+
+        $user = $owner_DAO->GetByEmail($_SESSION["email"]);
+
+        require_once VIEWS_PATH . "home_owner.php";
+    }
+}
