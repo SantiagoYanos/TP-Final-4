f321566a6b58e86803903e30eb4492e734c612da
Agustin <69488102+agustingk@users.noreply.github.com>
2022-10-11 10:17:07 -0300

    Add: PetController->Add()

--- Views/register_pet.php
+++ Views/register_pet.php
@@ -10,16 +10,16 @@
     <h1>Add Pet</h1>
     <h2>Add your pet info: </h2><br>
 
-    <form action="..." method="post">
+    <form action="<?php echo FRONT_ROOT . "Pet/Add" ?>" method="post">
         <label for="name">Name: </label>
         <input type="text" name="name" placeholder="Name"></br>
         <label for="">Breed: </label>
-        <input type="text" name="Breed" placeholder="Breed"></br>
+        <input type="text" name="breed" placeholder="Breed"></br>
        
         
         
         <label for="observations">Observations: </label>
-        <input type="text" name="observations" placeholder="observations"></br>
+        <input type="text" name="observation" placeholder="observations"></br>
         <label for="pet_size">Pet Size: </label>
         <select name="pet_size" id="pet_size">
             <option value="small">Small</option>
@@ -28,17 +28,10 @@
         </select></br>
 
         <label for="vaccination note"> vaccination note</label>
-        <input type="file" name=" vaccionation note" placeholder="submit vac note photo" > <br>
+        <input type="file" name=" vaccination_note" placeholder="submit vac note photo" > <br>
 
         <label for="photo/video"> Add photo/video</label>
-        <input type="file" name=" photo/video" placeholder="submit photo/video" > <br>
-
-      
-
-        
-
-
-    
+        <input type="file" name=" photo_video" placeholder="submit photo/video" > <br>
         <button type="submit">Add Pet</button>
     </form>
 </body>
