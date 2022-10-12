20a5282ce373ed6ba4a7b2997f8e93d46a213564
Manuel <manugrassi9@gmail.com>
2022-10-12 11:59:24 -0300

    login css

--- Views/login.php
+++ Views/login.php
@@ -5,12 +5,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="..\Views\css\utils.css">
     <title>Login</title>
 </head>
 
-<body style="background-image: url(../Views/images/ducky.jpg)">
+<body  style="background-image: url(../Views/images/ducky.jpg)">
+   
+    <div class="center">
     <h1>PET HERO</h1>
-    <div>
         <h2>WELCOME</h2>
         <form action="<?php echo FRONT_ROOT . "Auth/Login" ?>" method="post">
             <!-- formulario  login-->
@@ -34,5 +36,6 @@
 
     </div>
 </body>
+
 
 </html>
