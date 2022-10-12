276522328e93c69f463d85ac9d6cca572af1ac84
Agustin <69488102+agustingk@users.noreply.github.com>
2022-10-12 11:34:08 -0300

    Etc

--- Views/register_guardian.php
+++ Views/register_guardian.php
@@ -5,13 +5,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="..\Views\css\styleRegister.css">
+    <!--<link rel="stylesheet" href="..\Views\css\styleRegister.css">-->
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Nabla&display=swap" rel="stylesheet">
     <title>Guardian Register</title>
 </head>
 
-<body style="background-image: url(../Views/images/rick-roll.gif)">
+<body>
     <h1>Guardian Register</h1>
     <h2>Add your info: </h2><br>
 
