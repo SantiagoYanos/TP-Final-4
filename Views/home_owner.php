42edb2098c530dd9005d6fa541ee34c6c035f063
Agustin <69488102+agustingk@users.noreply.github.com>
2022-10-12 11:30:39 -0300

    Etc

--- Views/home_owner.php
+++ Views/home_owner.php
@@ -86,7 +86,6 @@
     </br></br>
 
     <div>
-
         <form action="<?php echo FRONT_ROOT . "Auth/logOut" ?>" method="post">
             <button type="logout">Logout</button></br>
         </form>
