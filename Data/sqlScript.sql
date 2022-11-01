4c1bdf155252ccd5d3be81c533ae20c3494748e9
Manuel <manugrassi9@gmail.com>
2022-11-01 09:57:40 -0300

    change: sql fix and update data grip checked

--- Data/sqlScript.sql
+++ Data/sqlScript.sql
@@ -1,16 +1,14 @@
 create database pet_hero;
 use pet_hero;
 
-create table pet_multimedia(
-    file_id bigint auto_increment,
-    file_path text not null,
-    pet_id bigint not null,
-    description varchar(150) not null,
-
-    constraint pk_pet_multimedia primary key (file_id),
-    constraint fk_pet_id foreign key (pet_id) references pets (pet_id) on delete cascade
-
+CREATE TABLE pet_sizes(
+    pet_size_id int auto_increment,
+    name varChar(50) not null,
+    CONSTRAINT pk_pet_size_id PRIMARY KEY (pet_size_id)
 );
+
+
+
 
 CREATE TABLE users(
     user_id bigint auto_increment,
@@ -23,29 +21,20 @@ CREATE TABLE users(
     birth_date date NOT NULL,
     active boolean not null default 1,
     CONSTRAINT pk_user_id PRIMARY KEY (user_id),
-    CONSTRAINT unq_email UNIQUE
+    CONSTRAINT unq_email UNIQUE (user_id)
 );
 
 create table owners(
     user_id bigint NOT NULL,
     dni bigint not null,
 
-    constraint pk_user_id primary key (user_id),
-    constraint fk_user_id foreign key (user_id) references users(user_id) ON DELETE CASCADE,
-    CONSTRAINT unq_user_id UNIQUE (user_id),
+    constraint pk_owner_user_id primary key (user_id),
+    constraint fk_owner_user_id foreign key (user_id) references users(user_id) ON DELETE CASCADE,
+
     constraint unq_dni unique (dni)
     /*constraint chk_birth_date check ( birth_date<=now() )*/
 );
 
-create table available_dates(
-    date_id bigint not null,
-    date date not null,
-    guardian_id bigint not null,
-
-    constraint pk_available_dates primary key (date_id),
-    /*constraint chk_future_date check (date>=now()),*/
-    constraint fk_guardian_id foreign key (guardian_id) references guardians (user_id) ON DELETE CASCADE
-);
 
 
 
@@ -57,14 +46,23 @@ create table guardians(
     preferred_size_cat int,
     price float,
 
-    constraint pk_user_id primary key (user_id),
-    CONSTRAINT fk_user_id foreign KEY (user_id) references users(user_id) ON DELETE CASCADE,
-    CONSTRAINT unq_user_id UNIQUE,
-    CONSTRAINT fk_preferred_size_dog FOREIGN KEY REFERENCES pet_sizes(pet_size_id) ON DELETE SET NULL,
-    CONSTRAINT fk_preferred_size_cat FOREIGN KEY REFERENCES pet_sizes(pet_size_id) ON DELETE SET NULL,
+    constraint pk_guardian_user_id primary key (user_id),
+    CONSTRAINT fk_guardian_user_id foreign KEY (user_id) references users(user_id) ON DELETE CASCADE,
+
+    CONSTRAINT fk_preferred_size_dog FOREIGN KEY (preferred_size_dog) REFERENCES pet_sizes(pet_size_id) ON DELETE SET NULL,
+    CONSTRAINT fk_preferred_size_cat FOREIGN KEY (preferred_size_cat) REFERENCES pet_sizes(pet_size_id) ON DELETE SET NULL,
     constraint unq_cuil unique (cuil),
     /*constraint chk_birth_date check ( birth_date<=now() ),*/
     constraint chk_price check (price>=0)
+);
+create table available_dates(
+    date_id bigint not null,
+    date date not null,
+    guardian_id bigint not null,
+
+    constraint pk_available_dates primary key (date_id),
+    /*constraint chk_future_date check (date>=now()),*/
+    constraint fk_guardian_id foreign key (guardian_id) references guardians (user_id) ON DELETE CASCADE
 );
 
 
@@ -81,6 +79,17 @@ create table pets(
     constraint pk_pet primary key (pet_id),
     CONSTRAINT fk_pet_size FOREIGN key (pet_size) REFERENCES pet_sizes(pet_size_id) ON DELETE SET NULL,
     constraint fk_owner_id foreign key (owner_id) references owners(user_id) on delete cascade
+);
+
+create table pet_multimedia(
+    file_id bigint auto_increment,
+    file_path text not null,
+    pet_id bigint not null,
+    description varchar(150) not null,
+
+    constraint pk_pet_multimedia primary key (file_id),
+    constraint fk_pet_id foreign key (pet_id) references pets (pet_id) on delete cascade
+
 );
 
 create table reservations(
@@ -127,9 +136,3 @@ create table payments(
     constraint fk_payment_reservation foreign key (reservation_id) references reservations (reservation_id),
     constraint chk_negative_amount check ( amount>=0 )
 );
-
-CREATE TABLE pet_sizes(
-    pet_size_id bigint auto_increment,
-    name varChar(50) not null
-    CONSTRAINT pk_pet_size_id PRIMARY KEY (pet_size_id)
-)
