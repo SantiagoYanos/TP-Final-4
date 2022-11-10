4ba5dc4eb4878aee52c8bb9c67bc0ef0ff0be913
Agustin <69488102+agustingk@users.noreply.github.com>
2022-11-10 11:30:41 -0300

    ASDASD

--- Controllers/ReservationController.php
+++ Controllers/ReservationController.php
@@ -45,22 +45,27 @@ class ReservationController
         }
 
         /////Chequear size
+        $PetDAO = new PetDAO();
         $guardianDAO = new GuardianDAO();
         $owner_DAO = new OwnerDAO();
         $guardian_user = $guardianDAO->GetById($guardian_id);
-        
-        foreach($pets_ids as $pet){
-            if($pet->gettype=="dog"){
+        $petList = array();
+
+        foreach($pets_ids as $pet_id){
+
+            $pet = $PetDAO->GetById($pet_id);
+            if($pet->getType() == "dog"){
                 if($guardian_user->getType_data()->getPreferred_size() > $pet->getSize()){
                     $flag=1;
                 }
             }
-            if($pet->gettype="cat"){
+            if($pet->getType()=="cat"){
                 if($guardian_user->getType_data()->getPreferred_size_cat() < $pet->getSize()){
                     $flag=1;
                 }
             }  
-        }                            
+        }
+                        
         
         if($flag==0){
                 $cant_pets = 0;
