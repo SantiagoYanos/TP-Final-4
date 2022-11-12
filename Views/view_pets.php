9e369faa2861bbcaa2aff7dc63d5ebea59c7e8e8
Yanos <santiago.yanos@gmail.com>
2022-11-12 14:21:39 -0300

    Add: ViewPets (ReservationsList)

--- Views/view_pets.php
+++ Views/view_pets.php
@@ -9,14 +9,14 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://kit.fontawesome.com/dec9278e05.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="..\Views\css\utils.css">
-    
+
     <title>view_pets</title>
 </head>
 
-<body class="bImageFix" style="background-image: url(../Views/images/gary.png) " >
+<body class="bImageFix" style="background-image: url(../Views/images/gary.png) ">
     <h1>My pets</h1>
 
-    <table class= "table table-striped table-bordered" style="text-align:center;" border="2">
+    <table class="table table-striped table-bordered" style="text-align:center;" border="2">
         <thead>
             <tr>
 
@@ -43,29 +43,29 @@
                     <td><?php echo $pet->getBreed() ?> </td>
                     <td><?php echo $pet->getType() ?> </td>
                     <td><?php echo $pet->getSize() ?></td>
-                    <td><img src="<?php echo "../" . IMG_PATH .  $pet->getId() . "/" . $pet->getVaccination_plan(); ?> " alt="vac note" height="100" width="100"></td> 
+                    <td><img src="<?php echo "../" . IMG_PATH .  $pet->getId() . "/" . $pet->getVaccination_plan(); ?> " alt="vac note" height="100" width="100"></td>
                     <td><?php echo $pet->getObservation() ?> </td>
                     <!--<td> <iframe width="786" height="442" src="https://www.youtube.com/embed/A6dhKpvhNKY?autoplay=1&controls=0&" </iframe> </td>-->
-                    <td> <a href=" <?php echo $pet->getPet_video();   ?>" alt="pet video"> Video </a>   </td>
+                    <td> <a href=" <?php echo $pet->getPet_video();   ?>" alt="pet video"> Video </a> </td>
 
                     <td>
-                        <form id= <?php echo $pet->getId(); ?> action="<?php echo FRONT_ROOT . "Pet/deletePet" ?>" method="post">
-                        <input type="hidden" name="petId" value="<?php echo $pet->getId() ?>"></input>
-                    </form>
-                    <button type="submit" class="mt-2" onclick="confirming(<?php echo $pet->getId(); ?>)">delete</button>
+                        <form id=<?php echo $pet->getId(); ?> action="<?php echo FRONT_ROOT . "Pet/deletePet" ?>" method="post">
+                            <input type="hidden" name="petId" value="<?php echo $pet->getId() ?>"></input>
+                        </form>
+                        <button type="submit" class="mt-2" onclick="confirming(<?php echo $pet->getId(); ?>)">delete</button>
 
-                    <script>
-                    function confirming(id) {
-                    var txt;
-                    if (confirm("are you sure you want to delete this pet?")) {
-                        txt = "You pressed OK!";
-                        document.getElementById(id).submit();
-                    } else {
-                        txt = "You pressed Cancel!";
-                    }
+                        <script>
+                            function confirming(id) {
+                                var txt;
+                                if (confirm("are you sure you want to delete this pet?")) {
+                                    txt = "You pressed OK!";
+                                    document.getElementById(id).submit();
+                                } else {
+                                    txt = "You pressed Cancel!";
+                                }
 
-                    }
-                    </script>
+                            }
+                        </script>
 
 
                     </td>
@@ -90,7 +90,7 @@
         <form action=<?php echo FRONT_ROOT . "Owner/HomeOwner" ?>>
             <button type="submit">Back</button>
         </form>
-   
+
 
 </body>
 
