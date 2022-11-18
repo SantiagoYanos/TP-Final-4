1861cbba31ce28a386dc054ef1a1db2e52ebd785
Yanos <santiago.yanos@gmail.com>
2022-11-18 12:19:36 -0300

    Add: PaymentDAO (SQL)

--- SQLDAO/PaymentDAO.php
+++ SQLDAO/PaymentDAO.php
@@ -3,9 +3,9 @@
 namespace SQLDAO;
 
 use SQLDAO\IModels as IModels;
-use SQLDAO\GuardianDAO as GuardianDAO;
-use SQLDAO\OwnerDAO as OwnerDAO;
-use SQLDAO\ReservationDAO as ReservationDAO;
+// use SQLDAO\GuardianDAO as GuardianDAO;
+// use SQLDAO\OwnerDAO as OwnerDAO;
+// use SQLDAO\ReservationDAO as ReservationDAO;
 
 class PaymentDAO implements IModels
 {
@@ -27,18 +27,17 @@ class PaymentDAO implements IModels
     public function GetById($id)
     {
         $query = "SELECT p.amount as 'amount', p.date as 'date', p.payment_number as 'payment_number', CONCAT(uo.name, ' ', uo.last_name) as 'owner_name', CONCAT(ug.name, ' ', ug.last_name) as 'guardian_name', r.price as 'price'
-
         FROM payments p 
         INNER JOIN users uo ON uo.user_id = p.owner_id
         INNER JOIN users ug ON ug.user_id = p.guardian_id
         INNER JOIN reservations r ON r.reservation_id = p.reservation_id
         WHERE p.payment_id = :payment_id;";
 
-        $params["payment_id"] = $id;
+        $parameters["payment_id"] = $id;
 
         $this->connection = Connection::GetInstance();
 
-        $resultSet = $this->connection->Execute($query, $params["payment_id"]);
+        $resultSet = $this->connection->Execute($query, $parameters);
 
         if (!$resultSet || $resultSet == []) {
             return null;
@@ -58,13 +57,53 @@ class PaymentDAO implements IModels
 
     public function Add($payment)
     {
+        $query = "INSERT INTO payments (amount, date, reservation_id, owner_id, guardian_id, payment_number) VALUES (:amount, now(), :reservation_id, :owner_id, :guardian_id, :payment_number)";
+
+        $parameters["amount"] = $payment->getAmount();
+        $parameters["reservation_id"] = $payment->getReservation_id();
+        $parameters["owner_id"] = $payment->getOwner_id();
+        $parameters["guardian_id"] = $payment->getGuardian_id();
+        $parameters["payment_number"] = $payment->getPayment_number();
+
+        $this->connection = Connection::GetInstance();
+
+        $resultSet = $this->connection->Execute($query, $parameters);
+
+        if (!$resultSet[0]) {
+            return null;
+        }
+        return $resultSet[0]["payment_id"];
     }
 
     public function GetByReservationId($reservation_id)
     {
-    }
+        $query = "SELECT p.amount as 'amount', p.date as 'date', p.payment_number as 'payment_number', CONCAT(uo.name, ' ', uo.last_name) as 'owner_name', CONCAT(ug.name, ' ', ug.last_name) as 'guardian_name', r.price as 'price'
 
-    public function LoadData()
-    {
+        FROM payments p 
+        INNER JOIN users uo ON uo.user_id = p.owner_id
+        INNER JOIN users ug ON ug.user_id = p.guardian_id
+        INNER JOIN reservations r ON r.reservation_id = p.reservation_id
+        WHERE r.reservation_id = :reservation_id;";
+
+        $parameters["reservation_id"] = $reservation_id;
+
+        $this->connection = Connection::GetInstance();
+
+        $resultSet = $this->connection->Execute($query, $parameters);
+
+        if (!$resultSet || $resultSet == []) {
+            return null;
+        }
+
+        $paymentArray = array();
+
+        $paymentArray["amount"] = $resultSet[0]["amount"];
+        $paymentArray["date"] = $resultSet[0]["date"];
+        $paymentArray["payment_number"] = $resultSet[0]["payment_number"];
+        $paymentArray["owner_name"] = $resultSet[0]["owner_name"];
+        $paymentArray["guardian_name"] = $resultSet[0]["guardian_name"];
+        $paymentArray["price"] = $resultSet[0]["price"];
+
+        return $paymentArray;
     }
 }
