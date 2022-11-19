64366da2b5e1c858c754a74da20a9cee6d9fc12a
Manuel <manugrassi9@gmail.com>
2022-11-19 20:41:33 -0300

    payment fiX

--- Views/show_payment.php
+++ Views/show_payment.php
@@ -12,6 +12,45 @@
 
 tu pago
 <?php var_dump($arrayPayment)  ?>
+
+<table class="table table-striped table-bordered" style="text-align:center;" border="2">
+        <thead>
+            <tr>
+
+                <th style="width: 150px;">amount</th>
+                <th style="width: 150px;">date</th>
+                <th style="width: 150px;">payment number</th>
+                <th style="width: 150px;">owner's name</th>
+                <th style="width: 150px;">guardian's name</th>
+                <th style="width: 150px;">price</th>
+          
+
+            </tr>
+
+        </thead>
+
+        <tbody>
+                <tr>
+                    <td><?php echo $arrayPayment["amount"] ?> </td>
+                    <td><?php echo $arrayPayment["date"] ?> </td>
+                    <td><?php echo $arrayPayment["payment_number"] ?> </td>
+                    <td><?php echo $arrayPayment["owner_name"] ?></td>
+                    <td><?php echo $arrayPayment["guardian_name"] ?></td>
+                    <td><?php echo $arrayPayment["price"] ?></td>
+
+                
+            
+                
+
+                </tr>
+
+        </tbody>
+
+
+
+
+
+    </table>
     
 
 <form action="<?php echo FRONT_ROOT . "Owner/ViewReservationsOwner" ?>" method="post">
