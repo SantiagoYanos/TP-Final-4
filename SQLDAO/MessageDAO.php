2034dbb6974f30a690cb0913a18e6e0ea1673ce8
Yanos <santiago.yanos@gmail.com>
2023-01-17 16:00:37 -0300

    Added: ChatV2 (v1) & comments deleted & message datetime

--- SQLDAO/MessageDAO.php
+++ SQLDAO/MessageDAO.php
@@ -39,18 +39,15 @@ class MessageDAO
     public function Add(Message $MessageSQL)
     {
         try {
-            $queryMessage = "insert into ". $this->tableName ." (description, sender_id, receiver_id, date) VALUES (:description, :sender_id, :receiver_id, now());";
+            $queryMessage = "insert into " . $this->tableName . " (description, sender_id, receiver_id, date) VALUES (:description, :sender_id, :receiver_id, now());";
 
             $parametersMessage["description"] = $MessageSQL->getDescription();
             $parametersMessage["sender_id"] = $MessageSQL->getSender();
             $parametersMessage["receiver_id"] = $MessageSQL->getReceiver();
-            
+
             $this->connection = Connection::GetInstance();
 
             $resultSet = $this->connection->ExecuteNonQuery($queryMessage, $parametersMessage);
-
-            
-            
         } catch (Exception $e) {
             throw $e;
         }
@@ -66,14 +63,40 @@ class MessageDAO
         $MessageSQL->setDescription($resultSet["description"]);
         return $MessageSQL;
     }
-    
-    public function GetByIds($senderId, $receiverId)
+
+    // public function GetByIds($senderId, $receiverId)
+    // {
+    //     try {
+
+    //         $messageList = array();
+
+    //         $query = "SELECT * FROM " . $this->tableName . " WHERE sender_id=:senderId AND receiver_id=:receiverId AND active=true ";
+
+    //         $parameters["senderId"] = $senderId;
+    //         $parameters["receiverId"] = $receiverId;
+
+    //         $this->connection = Connection::GetInstance();
+    //         $resultSet = $this->connection->Execute($query, $parameters);
+
+    //         foreach ($resultSet as $row) {
+
+    //             $messageSQL = $this->LoadData($row);
+
+    //             array_push($messageList, $messageSQL);
+    //         }
+
+    //         return $messageList;
+    //     } catch (Exception $e) {
+    //         throw $e;
+    //     }
+    // }
+
+    public function GetByIdsV2($senderId, $receiverId)
     {
         try {
-
             $messageList = array();
 
-            $query = "SELECT * FROM " . $this->tableName . " WHERE sender_id=:senderId AND receiver_id=:receiverId AND active=true ";
+            $query = "SELECT * FROM " . $this->tableName . " WHERE ((sender_id=:senderId AND receiver_id=:receiverId) OR (sender_id=:receiverId AND receiver_id=:senderId)) AND active=true ORDER BY date ASC;";
 
             $parameters["senderId"] = $senderId;
             $parameters["receiverId"] = $receiverId;
