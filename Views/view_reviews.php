8fc327791837e9259ddda300ad5f1f4f72533b31
Manuel <manugrassi9@gmail.com>
2023-01-17 16:52:54 -0300

    updateViewReviews

--- Views/view_reviews.php
+++ Views/view_reviews.php
@@ -7,28 +7,31 @@
     <title>Reviews</title>
 </head>
 <body>
-    <h1>Reviews from <?php $guardian->getName() ?></h1>
+    <h1>Reviews from the guardian </h1>
     
     
     <table class="table table-striped table-bordered" style="text-align:center; font-weight:bold" border="2">
         <thead>
             <tr>
-                <th style="width: 150px;">Name: </th>
-                <th style="width: 150px;">Comment: </th>
-                <th style="width: 150px;">Rating: </th>
-                <th style="width: 150px;">Date: </th>
+                <th style="width: 150px;">Name </th>
+                <th style="width: 150px;">Comment </th>
+                <th style="width: 150px;">Rating </th>
+                <th style="width: 150px;">Date </th>
             </tr>
 
         </thead>
 
         <tbody>
+            
             <?php foreach ($total as $review) 
             { ?>
+                <tr>
                 <td style="width: 150px;"><?php echo $review->getOwner_name() ?></td>
-                <td style="width: 150px;"><?php echo ".$review->getComment()." ?></td>
+                <td style="width: 150px;"><?php echo $review->getComment() ?></td>
                 <td style="width: 150px;"><?php echo $review->getRating() ?></td>
                 <td style="width: 150px;"><?php echo $review->getDate() ?></td>
             <?php } ?>
+            </tr>
         </tbody>
 
     </table>
